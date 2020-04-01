9c0378906b4de3edd2c862dd52fa65450ed536e1
Logan Brown <lkbrownvb@gmail.com>
2020-04-01 21:05:21 +0000

    I updated thing Y and added thing D

---
+++ logans-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.imgur.com/cL2HoqR.png" class="App-logo" alt="logo" />
+        <p>
+        Whatcha lookin @ mate
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
